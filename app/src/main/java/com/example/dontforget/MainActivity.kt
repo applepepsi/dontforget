46c43377fcdd987f4af4ba667422cd35f6f8059c
applepepsi <dlaejrtnz@gmail.com>
2023-12-19 01:27:49 +0900

    터치효과 추가

--- app/src/main/java/com/example/dontforget/MainActivity.kt
+++ app/src/main/java/com/example/dontforget/MainActivity.kt
@@ -55,7 +55,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.scheduleViewer.adapter=scheduleAdapter
         binding.scheduleViewer.layoutManager=LinearLayoutManager(this@MainActivity)
-        binding.scheduleViewer.addItemDecoration(ItemSpacingController(5))
+        binding.scheduleViewer.addItemDecoration(ItemSpacingController(0))
 
         val itemTouchHelper = ItemTouchHelper(SwipeToDeleteCallback(object : SwipeToDeleteCallback.OnSwipeListener {
             override fun onSwipe(position: Int) {
